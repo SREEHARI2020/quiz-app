67187540b1c7af233037015d531416c63ebe8a47
SREEHARI <sreehari2610@gmail.com>
2020-11-23 17:54:55 +0530

    finalized

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	const questions = [
+		{
+			questionText: 'What is the capital of France?',
+			answerOptions: [
+				{ answerText: 'New York', isCorrect: false },
+				{ answerText: 'London', isCorrect: false },
+				{ answerText: 'Paris', isCorrect: true },
+				{ answerText: 'Dublin', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Who is CEO of Tesla?',
+			answerOptions: [
+				{ answerText: 'Jeff Bezos', isCorrect: false },
+				{ answerText: 'Elon Musk', isCorrect: true },
+				{ answerText: 'Bill Gates', isCorrect: false },
+				{ answerText: 'Tony Stark', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'The iPhone was created by which company?',
+			answerOptions: [
+				{ answerText: 'Apple', isCorrect: true },
+				{ answerText: 'Intel', isCorrect: false },
+				{ answerText: 'Amazon', isCorrect: false },
+				{ answerText: 'Microsoft', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'How many Harry Potter books are there?',
+			answerOptions: [
+				{ answerText: '1', isCorrect: false },
+				{ answerText: '4', isCorrect: false },
+				{ answerText: '6', isCorrect: false },
+				{ answerText: '7', isCorrect: true },
+			],
+		},
+	];
+  const [count,setcount]=useState(0);
+  const[score,setscore]=useState(false);
+  const[mark,setmark]=useState(0);
+  const handleClick=(isCorrect)=>{
+   
+   if(isCorrect){
+    setmark(mark+1);
+
+   }
+   
+    const nextstate = count + 1;
+   if(
+     nextstate<questions.length)
+   {setcount(nextstate);} 
+ 
+  else
+  {
+    setscore(true);
+    
+  }
 }
-
-export default App;
+const resetvalues=()=>{
+  setcount(0)
+  setmark(0);
+  setscore(false);
+}
+	return (
+		<div className='app'>
+			{/* HINT: replace "false" with logic to display the 
+      score when the user has answered all the questions */}
+			{score ? (
+			<>	<div className='score-section'>You scored {mark} out of {questions.length}
+       <button onClick={resetvalues}>RESET</button>
+      </div>
+       
+        </>
+			) : (
+				<>
+					<div className='question-section'>
+						<div className='question-count'>
+      <span>Question {count + 1}</span>/{questions.length}
+						</div>
+						<div className='question-text'>{questions[count].questionText}</div>
+					</div>
+					<div className='answer-section'>
+					{
+            questions[count].answerOptions.map((answer)=>{
+              return(<button onClick={()=>handleClick(answer.isCorrect)}>{answer.answerText}</button>);
+             
+            })
+           
+            
+          }
+					</div>
+				</>
+			)}
+		</div>
+	);
+}
